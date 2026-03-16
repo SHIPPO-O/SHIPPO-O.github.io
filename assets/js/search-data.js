9249d06a850bd41791a6313d6b9d20c359128596
SHIPPO-O <hwanbum4056@gmail.com>
2026-03-16 04:59:27 +0000

    Deploying to gh-pages from @ SHIPPO-O/SHIPPO-O.github.io@a621d2d8033087254ae99495a4ac6ec76186f452 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -493,18 +493,11 @@ ninja.data = [{
           window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/Hwan Beom Kim", "_blank");
         },
       },{
         id: 'social-scholar',
@@ -512,13 +505,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
